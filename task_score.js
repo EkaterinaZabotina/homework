940b473dc8968a0ee96df3addb802d9baed36652
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-09 11:59:15 +0300

    used method slice - hw-2-1-4

--- task_score.js
+++ task_score.js
@@ -1,9 +1,11 @@
 let studentName = "Katya"
 let studentSurname = "Zabotina"
 
+let shortName = studentName.slice (0,1)
+
 let introModuleScore = 80
 let gitModuleScore = 90
 let jsModuleScore = 100
 
 let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
-console.log(`The average score for ${studentName} ${studentSurname} is ${averageScore}`)
+console.log(`The average score for ${shortName}. ${studentSurname} is ${averageScore}`)
