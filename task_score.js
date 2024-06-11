f52eed797d2c62b3a1bb2426a94d3ed774e64cbe
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-11 18:18:13 +0300

    added file with average score - hw 2-1-3

--- task_score.js
+++ task_score.js
@@ -1,11 +1,13 @@
-let studentName = "Katya"
-let studentSurname = "Zabotina"
+let studentName = "Katya";
+let studentSurname = "Zabotina";
 
-let shortName = studentName.slice (0,1)
+let shortName = studentName.slice(0, 1);
 
-let introModuleScore = 80
-let gitModuleScore = 90
-let jsModuleScore = 100
+let introModuleScore = 80;
+let gitModuleScore = 90;
+let jsModuleScore = 100;
 
-let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
-console.log(`The average score for ${shortName}. ${studentSurname} is ${averageScore}`)
+let averageScore = (introModuleScore + gitModuleScore + jsModuleScore) / 3;
+console.log(
+  `The average score for ${shortName}. ${studentSurname} is ${averageScore}`
+);
