9616ebdadb93c5f639bfb99d1616cac49805377a
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-17 14:16:58 +0300

    added file with player`s max score

---
+++ taskscore.js
@@ -0,0 +1,6 @@
+let introModuleScore = 80
+let gitModuleScore = 90
+let jsModuleScore = 100
+
+let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
+console.log(`The average score for ${studentName}. ${studentSurname} is ${averageScore}`)
